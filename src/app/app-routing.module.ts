47cd1d755b6ea2bb7af1c4bd2a59d22151d8eb68
Golge07 <erenbulut0707@gmail.com>
2023-09-28 18:04:00 +0300

    menu bug

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,13 +3,17 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
   {
-    path: 'home',
-    loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)
+    path: '',
+    redirectTo: 'page1',
+    pathMatch: 'full'
   },
   {
-    path: '',
-    redirectTo: 'home',
-    pathMatch: 'full'
+    path: 'page1',
+    loadChildren: () => import('./pages/page1/page1.module').then( m => m.Page1PageModule)
+  },
+  {
+    path: 'page2',
+    loadChildren: () => import('./pages/page2/page2.module').then( m => m.Page2PageModule)
   },
 ];
 
